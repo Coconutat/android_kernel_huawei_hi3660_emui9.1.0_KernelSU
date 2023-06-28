a61583a7bacd8ed1a00800ae22a382a2b72ab6a1
Coconut <coconutat@protonmail.com>
2023-06-28 14:23:00 +0800

    security/selinux/selinuxfs.c:Make it Always  run into permissive mode

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -163,6 +163,9 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+	/*Modify for Huawei.This value for enforce the permissive mode.*/
+  	new_value = 0;
+
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
